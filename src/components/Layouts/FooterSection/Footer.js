a186e91b21adf98443a15e3d7ac2cd6917e12bfb
Leo Bernardo <leo.olivbernardo@gmail.com>
2022-12-23 16:39:52 -0300

    commit_final_23/12

--- src/components/Layouts/FooterSection/Footer.js
+++ src/components/Layouts/FooterSection/Footer.js
@@ -15,6 +15,7 @@ function Footer(){
               <li><a href='https://www.instagram.com/lobernardo/' target= '_blank'><FaInstagram /></a></li>
               <li><a href='https://www.linkedin.com/in/leonardo-obernardo/' target= '_blank'><FaLinkedin /></a></li>
             </ul>
+            <p className={styles.copy}><span>E-mail:</span><a>leo.olivbernardo@gmail.com</a></p>
             <p className={styles.copy}>
               <span>Portfóio</span>  &copy;  2022</p>
           </footer>
